51fc9fd66a8e1d099885dcaf0e67dc05d654fe9b
Alexei Prokudin <prokudin@jlab.org>
2018-06-12 17:16:36 -0400

    hermes

---
+++ inputs/upol_hermes_alexei.py
@@ -0,0 +1,81 @@
+conf={}
+
+############################################################################
+# resouce allocation
+
+conf['ncpus']=1
+
+############################################################################
+# maxlike setup
+
+conf['screen mode']='plain'
+#conf['screen mode']='curses'
+
+############################################################################
+# mcsetup
+
+conf['method']='kde'
+conf['itmax']=None
+conf['tol']=1e-6
+conf['kde bw']=None
+conf['num points factor']=10
+
+
+############################################################################
+ 
+############################################################################
+# params
+
+conf['params']={}
+conf['basis']='default'
+
+
+conf['params']['pdf']={}
+conf['params']['pdf']['widths0 valence']  = {'value':<<    5.23523467386036500670e-01>>,'fixed':False,'min':0,'max':10}
+conf['params']['pdf']['widths0 sea']      = {'value':<<    5.79275645760695900854e-01>>,'fixed':False,'min':0,'max':10}
+
+conf['params']['ff']={}
+conf['params']['ff']['widths0 pi+ fav']   = {'value':<<    1.24066238734244951858e-01>>,'fixed':False,'min':0,'max':1}
+conf['params']['ff']['widths0 pi+ unfav'] = {'value':<<    1.43610957318586640596e-01>>,'fixed':False,'min':0,'max':1}
+conf['params']['ff']['widths0 k+ fav']    = {'value':<<    1.34331584518304875120e-01>>,'fixed':False,'min':0,'max':1}
+conf['params']['ff']['widths0 k+ unfav']  = {'value':<<    1.99535236959634459941e-01>>,'fixed':False,'min':0,'max':1}
+
+
+
+############################################################################
+# set data sets
+
+conf['datasets']={}
+
+# SIDIS
+
+conf['datasets']['sidis']={}
+
+
+conf['datasets']['sidis']['filters']={}
+conf['datasets']['sidis']['filters'][0]={}
+conf['datasets']['sidis']['filters'][0]['idx']=[1000,1001,1004,1005,1002,1003,1006,1007]
+#conf['datasets']['sidis']['filters'][0]['filter']="z<0.6 and Q2>1.69 and pT>0.2 and pT<0.9" # npts    = 978 chi2    = 1206.873681
+#conf['datasets']['sidis']['filters'][0]['filter']="z>0.2 and z<0.6 and Q2>1.69 and (pT/z)**2<0.25*Q2" # rapidity Gunar wrote z< 0.2 and z>0.8 are padding bins
+conf['datasets']['sidis']['filters'][0]['filter']="Q2>1.69 and z>0.2 and z<0.6 and pT>0.2 and pT<0.9" 
+
+conf['datasets']['sidis']['xlsx']={}
+
+conf['datasets']['sidis']['xlsx'][1000]='sidis/expdata/1000.xlsx'  # |  proton   | pi+    | M_Hermes | hermes 
+conf['datasets']['sidis']['xlsx'][1001]='sidis/expdata/1001.xlsx'  # |  proton   | pi-    | M_Hermes | hermes 
+conf['datasets']['sidis']['xlsx'][1004]='sidis/expdata/1004.xlsx'  # |  deuteron | pi+    | M_Hermes | hermes 
+conf['datasets']['sidis']['xlsx'][1005]='sidis/expdata/1005.xlsx'  # |  deuteron | pi-    | M_Hermes | hermes 
+
+conf['datasets']['sidis']['xlsx'][1002]='sidis/expdata/1002.xlsx'  # |  proton   | k+    | M_Hermes | hermes 
+conf['datasets']['sidis']['xlsx'][1003]='sidis/expdata/1003.xlsx'  # |  proton   | k-    | M_Hermes | hermes 
+conf['datasets']['sidis']['xlsx'][1006]='sidis/expdata/1006.xlsx'  # |  deuteron | k+    | M_Hermes | hermes 
+conf['datasets']['sidis']['xlsx'][1007]='sidis/expdata/1007.xlsx'  # |  deuteron | k-    | M_Hermes | hermes 
+
+conf['datasets']['sidis']['norm']={}
+for k in conf['datasets']['sidis']['xlsx']: conf['datasets']['sidis']['norm'][k]={'value':1,'fixed':True,'min':0,'max':1} 
+
+
+
+
+
+
